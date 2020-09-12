395cd802162ad20cef8eefee5ce0ea061dce8704
nabil-maher <nabilmaher11@gmail.com>
2020-09-12 18:28:32 +0200

    Add files via upload

---
+++ Netflix-master/js/Links.js
@@ -0,0 +1,198 @@
+///Shaft 
+$('#movie_486131').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=d096eabe715ddb85de279129cb590a63&img=https://image.tmdb.org/t/p/original/kBuenNbs1SidHPxJuMyMU8JMboI.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///DangerousLies 
+$('#movie_689723').magnificPopup({
+	items: {
+		src: 'https://s4.watch4unow.com/player/main_player1.php?gdrive_ids=Zm96bVZpTUdRWGJVczVsZDFXWnNFOFJyNGVtYXVIQWZ5UzFlYmt4eVlXdHVRZlMzR1RNblVEZkgzY2UzdUljcg,,&hls_id=b300292c24e754cd33ca8b612b006f57&img=tuqv0jb0o7sBOivgkagKJHuO6X4.jpg&bkdrp=true&img=tuqv0jb0o7sBOivgkagKJHuO6X4.jpg&bkdrp=true&backup_data=K1NEcm83RnA5YXVNa1RYd09CNFk4RHQvQkF0azM4V1VTSXZ6ZS9veTVQUCtQcFRmdXdUcldKV2FMVTBITGd2WVJBeWI1L3p4dFViUUxSTmFNMzJ5ZmdYa2ttTHQ1VnYzblIwWE5JMmlvREFZZnRaSGdaM1g2dHN6ZWRLQVBGT3NpNFYvSDVvd0lJVWlXeXhzRlBDTWFBUVluZXo1dldaZ3lVV1lqWEJIL0F1dFFJVGppTmIyOTNIcTZtdnVhVG5CZ0l5dTlOZTV5WFE4aW5IK0FWZW9PQT09'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///I Am Not Okay About This  
+
+///E1
+
+$('#episode_1816649').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=8012810bc839e4e0d1f3e571f91d74ad&img=https://image.tmdb.org/t/p/original/zXTjQATiqqPtQoGNCuwOftzX9nE.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E2
+
+$('#episode_2174344').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=37b6e6160cc50128629409b7376b6a46&img=https://image.tmdb.org/t/p/original/nHPUZaq3zSnLGvi0kb63BoiQfwE.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E3
+
+$('#episode_2174345').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=278b560f85ca84fcf8a0c3fc7a3faa96&img=https://image.tmdb.org/t/p/original/emo324cObvPjwncerL31GuFgjsk.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E4
+
+$('#episode_2174346').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=1aa492f8b0bc540bfa75e745974c99af&img=https://image.tmdb.org/t/p/original/6CxrkwEJbVSWtPhmseb5vghQbnF.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E5
+
+$('#episode_2174347').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=fc040420ac570e0bc814b13c0e0992b4&img=https://image.tmdb.org/t/p/original/mT1GxVeo3MtyBqcJJpBiLMMSC4e.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E6
+
+$('#episode_2174348').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=b028217fd2ca69ffb07b988d0e8fded4&img=https://image.tmdb.org/t/p/original/egIdyBTRPhsdmrb9MKk1EJFKjpN.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E7
+
+$('#episode_2174350').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=675d74d2192129cce56eea4ed283c8ad&img=https://image.tmdb.org/t/p/original/opwG6izph7vR1Z6OurvcZ3kdHKo.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///Self Made
+
+///E1
+
+$('#episode_2043464').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=6dfa7ce8fcc47a4beef2a812f6aea8a8&img=https://image.tmdb.org/t/p/original/taNTSW56dBkWBpf2BXs1qNbrKKA.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E2
+
+$('#episode_2043466').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=448907a38313d5513bc05f7ec6f1124a&img=https://image.tmdb.org/t/p/original/ubN8PWaqRhiaJ1zHhEdarUiqFGb.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E3
+
+$('#episode_2043468').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=336d5d03f5bfaebfa0f128bd0f36baa9&img=https://image.tmdb.org/t/p/original/xaJe9nwZ15d53oE94YDeQt8pol5.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+///E4
+
+$('#episode_2043470').magnificPopup({
+	items: {
+		src: 'https://embed.faselhdstream.com/player.php?vid=4d6e03fb57046de0cab41e7ab6ccd92d&img=https://image.tmdb.org/t/p/original/1ZR2Fg0Til5SwcPD1vKFXt7sIwS.jpg'
+	},
+	type: 'iframe',
+	iframe: {
+		markup: '<div class="mfp-iframe-scaler">' +
+			'<div class="mfp-close"></div>' +
+			'<iframe class="mfp-iframe" sandbox="allow-scripts allow-same-origin" frameborder="0" allowfullscreen></iframe>' +
+			'</div>',
+	}
+})
+
+
